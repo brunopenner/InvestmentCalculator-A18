7e14d1d4d148aab271ca737411cb3d25ea0ca98c
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-06-30 15:42:13 +0800

    Creating Header component

---
+++ src/app/reference-component/reference-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reference-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './reference-component.component.html',
+  styleUrl: './reference-component.component.css'
+})
+export class ReferenceComponentComponent {
+
+}
