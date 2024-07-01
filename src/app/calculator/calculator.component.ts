60430740de8a2b284ca3d00399184dbf621b82d5
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-07-01 22:12:11 +0800

    calculator component

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'app-calculator',
+    standalone: true,
+    templateUrl: './calculator.component.html',
+    styleUrl: './calculator.component.css',
+    imports: []
+})
+
+export class CalculatorComponent {}
