4c0a409b30479adcc4ff0c9e6cc2235907565a84
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-07-01 22:24:47 +0800

    Results Component Barebones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { ResultsComponent } from './results/results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, CalculatorComponent]
+  imports: [HeaderComponent, CalculatorComponent, ResultsComponent]
 })
 export class AppComponent {}
